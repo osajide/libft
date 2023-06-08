6e5ed77ccec280f5adb4e158b2cdd42aa2626f6a
osajide <osajide5@gmail.com>
2023-06-08 15:35:36 +0100

    strjoin modified

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: osajide <osajide@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/09 19:28:06 by osajide           #+#    #+#             */
-/*   Updated: 2022/10/23 08:56:43 by osajide          ###   ########.fr       */
+/*   Updated: 2023/06/08 15:35:06 by osajide          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,10 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	char	*join;
 
-	if (!s1 || !s2)
-		return (NULL);
+	if (!s1)
+		s1 = ft_calloc(1, 1);
+	if (!s2)
+		return ((char *)s1);
 	join = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
 	if (!join)
 		return (NULL);
@@ -31,11 +33,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 		i++;
 	}
 	while (((char *)s2)[j])
-	{
-		join[i] = ((char *)s2)[j];
-		i++;
-		j++;
-	}
+		join[i++] = ((char *)s2)[j++];
 	join[i] = '\0';
 	return (join);
 }
